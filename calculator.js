05f5b3f47da470fe78c799dc30cd1a5a75345688
Jose Chavez <joseacb144@gmail.com>
2020-06-10 03:55:08 -0500

    Finalize project for now.

--- calculator.js
+++ calculator.js
@@ -27,8 +27,8 @@ app.get("/bmicalculator", function(req, res){
 
 app.post("/bmicalculator", function(req, res){
   //body-parser is used to retreive number inputs for bmi
-  var weight = Number(req.body.weight);
-  var height = Number(req.body.height);
+  var weight = parseFloat(req.body.weight);
+  var height = parseFloat(req.body.height);
   var result = weight/(height*height);
 
   res.send("Your BMI is: " + result);
